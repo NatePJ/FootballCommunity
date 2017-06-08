fbe0caa335245a3c2372a49b1e25e098c95deb89
PJay <heyheypjay@gmail.com>
2017-06-09 07:08:31 +0800

    download and upload file

--- app/src/main/java/com/bp/footballcommunity/activities/PublishFootballActivity.java
+++ app/src/main/java/com/bp/footballcommunity/activities/PublishFootballActivity.java
@@ -25,7 +25,17 @@ import com.bp.footballcommunity.MainActivity;
 import com.bp.footballcommunity.R;
 import com.bp.footballcommunity.adapters.AddPictureAdapter;
 import com.bp.footballcommunity.utilities.BitmapUtils;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.apache.http.Header;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -159,9 +169,99 @@ public class PublishFootballActivity extends Activity{
                     //TODO 修改
 //                    Log.d(TAG,)
 //                    photoPath = cursor.getString(column_index);
+                    /**
+                     * 上传图片
+                     */
+                    String[] proj = { MediaStore.Images.Media.DATA };
+
+                    Cursor actualimagecursor = managedQuery(uri,proj,null,null,null);
+
+                    int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                    actualimagecursor.moveToFirst();
+
+                    final String img_path = actualimagecursor.getString(actual_image_column_index);
+
+
+
+                    String url = "http://192.168.137.1:8080/SSHProject/file.png";
+                    Log.d("downloadpicfile","1");
+                    AsyncHttpClient client = new AsyncHttpClient();
+                    Log.d("downloadpicfile","2");
+                    client.get(url, new FileAsyncHttpResponseHandler(this) {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, File response) {
+                            try {//此try-catch块中的代码将下载到的文件保存在一个目录下
+                                // Context.getFilesDir()，该方法返回/data/data/[youPackageName]/files的File对象
+                                FileOutputStream fileOutputStream = new FileOutputStream(
+                                        PublishFootballActivity.this.getFilesDir().getPath() + "/file.png");
+                                Log.d("downloadpicfile",PublishFootballActivity.this.getFilesDir().getPath() + "/file.png");
+                                FileInputStream fileInputStream = new FileInputStream(response);
+                                byte[] bytes = new byte[(int) response.length()];
+                                fileInputStream.read(bytes);
+                                fileOutputStream.write(bytes);
+                                Log.d("downloadpicfile",PublishFootballActivity.this.getFilesDir().getPath() + "/file.png");
+                                fileOutputStream.flush();
+                                fileInputStream.close();
+                                fileOutputStream.close();
+                            } catch (Exception e) {
+                                Log.d("downloadpicfile","fail");
+                                e.printStackTrace();
+                            }
+
+                            /*********do something ...... ********/
+                            /***************上传部分***************/
+                            /*********do something ...... ********/
+                /*上传部分*/
+                            //找到上传的靶文件（此时的靶文件应该是我们刚刚下载到的文件）
+//                File file = new File(UpLoadActivity.this.getFilesDir().getPath(),"file.png");
+                            File file1 = new File("/data/user/0/com.bp.footballcommunity/files/headimage6.png");
+
+                            File file = new File("/storage/emulated/0/Pictures/headimage6.png");
+                            Log.d("downloadpicfile",PublishFootballActivity.this.getFilesDir().getPath());
+                            Log.d("downloadpicfile0", String.valueOf(file));
+                            Log.d("downloadpicfile1", String.valueOf(file1));
+                            //封装待发送参数（以键值对形式），即靶文件，内容类型设置为multipart/form-data
+                            RequestParams params = new RequestParams();
+                            try {
+                                params.put("file", file, "multipart/form-data");
+                                params.put("filename","headimage6.png");
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            }
+                            //没错，又是它
+                            AsyncHttpClient uploader = new AsyncHttpClient();
+
+                            //这次是post请求，看清楚，多了第二个参数params，其实也就是被封装的靶文件啦
+                            //这次使用的响应体接收者是AsyncHttpResponseHandler，回调处理也简单，就是现实上传成功与否
+                            Log.d("uploadpicfile", "upload test");
+                            uploader.post("http://192.168.137.1:8080/SSHProject/uploadpicture", params,
+                                    new AsyncHttpResponseHandler() {
+                                        @Override
+                                        public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+                                            Log.d("uploadpicfile", "upload success");
+
+                                        }
+                                        @Override
+                                        public void onFailure(int arg0, Header[] arg1, byte[] arg2, Throwable arg3) {
+                                            Log.d("uploadpicfile", "upload failure");
+
+                                        }
+                                    });
+
+                        }
+                        //下载失败时回调onFailure方法
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                            Log.i("file", "failure" + statusCode);
+                        }
+                    });
+                    /**
+                     * 上传图片
+                     */
                     photoPath = getRealPathFromURI(uri);
                     mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uri);
-                    Log.d(TAG, String.valueOf(mBitmap));
+                    Log.d(TAG, String.valueOf(uri));
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
